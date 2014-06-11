686bf352352d6adb2835f54d64c0ff3a692b8bc5
chank <fangliquan@qq.com>
2014-06-11 16:54:06 +0800

    add parse sub project and library dependency and solve the deprecated cacheDirectory problem

--- src/main/scala/com/qifun/sbtHaxe/HaxePlugin.scala
+++ src/main/scala/com/qifun/sbtHaxe/HaxePlugin.scala
@@ -20,24 +20,25 @@ final object HaxePlugin extends Plugin {
 
   /**
    *  默认的[[haxe]]任务设置值。
-   *  
+   *
    *  当编译Haxe时，某些所需的[[sbt.SettingKey]]是Sbt内置设置。
    *  所以这些[[sbt.SettingKey]]会使用`haxeConfiguration`参数传入[[Haxe]]或[[TestHaxe]]作为[[sbt.Configuration]]以便区分。
    *  例如`sourceDirectory`、`dependencyClasspath`等。
-   *  
+   *
    *  另一些[[sbt.SettingKey]]是[[HaxePlugin]]插件专用设置，不会与内置设置同名。
    *  所以这些设置不用自定义的[[sbt.Configuration]]就可以区分，而用`injectConfiguration`参数传入[[Compile]]或[[Test]]作为[[sbt.Configuration]]。
    *  例如`haxeCommand`、`haxeOptions`等。
-   *  
+   *
    *  @param haxeConfiguration 当把Sbt内置[[sbt.SettingKey]]重用于Haxe编译时所需的[[sbt.Configuration]]。
    *  @param injectConfiguration [[HaxePlugin]]插件专有[[sbt.SettingKey]]所用的Sbt内置[[sbt.Configuration]]。
-   */ 
+   */
   final def haxeSetting(
     haxeConfiguration: Configuration,
     injectConfiguration: Configuration) = {
     haxe in injectConfiguration := {
       val includes = (dependencyClasspath in haxeConfiguration).value
-      val cache = (cacheDirectory in haxeConfiguration).value
+      val cache = (streams in haxeConfiguration).value.cacheDirectory
+      
       val cachedTranfer = FileFunction.cached(cache / "haxe", inStyle = FilesInfo.lastModified, outStyle = FilesInfo.exists) { (in: Set[File]) =>
         IO.withTemporaryDirectory { temporaryDirectory =>
           val deps = (buildDependencies in haxeConfiguration).value.classpath((thisProjectRef in haxeConfiguration).value)
@@ -47,7 +48,7 @@ final object HaxePlugin extends Plugin {
             ResolvedClasspathDependency(dep: ProjectRef, _) <- deps
           } yield {
             dep match {
-              case ProjectRef(path, subProject) => (path.getPath().substring(1) + subProject + "/src/haxe").replace("/", System.getProperty("file.separator"))
+              case ProjectRef(path, subProject) => (path.getPath.substring(1) + subProject + "/src/haxe").replace("/", System.getProperty("file.separator"))
               case _ => ""
             }
           }
@@ -59,11 +60,18 @@ final object HaxePlugin extends Plugin {
           }
           (streams in haxeConfiguration).value.log.debug("dependency path: " + jarPathes)
 
+          // Add the haxe project's source path in order to build the test-haxe
+          val testProjectPath:Seq[String] = haxeConfiguration.name match {
+            case "test-haxe" => Seq("-cp", (baseDirectory { _ / "src" / "haxe" }).value.toString)
+            case _ => Seq()
+          }
+
           val processBuilder =
             Seq[String](
               (haxeCommand in injectConfiguration).value,
               "-cp", (sourceDirectory in haxeConfiguration).value.getPath) ++
               (Seq[String]() /: dependSources)(_ ++ Seq("-cp", _)) ++
+              testProjectPath ++
               (Seq[String]() /: jarPathes)(_ ++ Seq("-java-lib", _)) ++
               Seq("-java", temporaryDirectory.getPath,
                 "-D", "no-compilation") ++
