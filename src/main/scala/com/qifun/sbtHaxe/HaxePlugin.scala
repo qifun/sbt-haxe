aa3009b82f14b659db1cf49ed7099d53603e65d4
杨博 <pop.atry@gmail.com>
2014-06-11 11:49:30 +0800

    优化注释

--- src/main/scala/com/qifun/sbtHaxe/HaxePlugin.scala
+++ src/main/scala/com/qifun/sbtHaxe/HaxePlugin.scala
@@ -23,15 +23,15 @@ final object HaxePlugin extends Plugin {
    *  默认的[[haxe]]任务设置值。
    *  
    *  当编译Haxe时，某些所需的[[sbt.SettingKey]]是Sbt内置设置。
-   *  所以这些[[sbt.SettingKey]]会使用`haxeConfiguration`参数传入[[Haxe]]或[[TestHaxe]]作为前缀以便区分。
+   *  所以这些[[sbt.SettingKey]]会使用`haxeConfiguration`参数传入[[Haxe]]或[[TestHaxe]]作为[[sbt.Configuration]]以便区分。
    *  例如`sourceDirectory`、`dependencyClasspath`等。
    *  
    *  另一些[[sbt.SettingKey]]是[[HaxePlugin]]插件专用设置，不会与内置设置同名。
-   *  所以这些设置不用自定义的前缀就可以区分，而用`injectConfiguration`参数传入[[Compile]]或[[Test]]作为前缀。
+   *  所以这些设置不用自定义的[[sbt.Configuration]]就可以区分，而用`injectConfiguration`参数传入[[Compile]]或[[Test]]作为[[sbt.Configuration]]。
    *  例如`haxeCommand`、`haxeOptions`等。
    *  
-   *  @param haxeConfiguration 当把Sbt内置[[sbt.SettingKey]]重用于Haxe编译时所需的前缀。
-   *  @param injectConfiguration [[HaxePlugin]]插件专有[[sbt.SettingKey]]所用的内置前缀。
+   *  @param haxeConfiguration 当把Sbt内置[[sbt.SettingKey]]重用于Haxe编译时所需的[[sbt.Configuration]]。
+   *  @param injectConfiguration [[HaxePlugin]]插件专有[[sbt.SettingKey]]所用的Sbt内置[[sbt.Configuration]]。
    */ 
   final def haxeSetting(
     haxeConfiguration: Configuration,
